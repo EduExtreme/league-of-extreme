8920972547a83fa9780f3e8624ab317862a5c525
Eduardo André <edugexp@gmail.com>
2023-08-25 17:46:29 -0400

    feat: add zustand, made last types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import PlayerStatus from "../components/PlayerStats";
 export default function Home() {
   return (
     <main>
-      <PlayerStatus playerName="name" />
+      <PlayerStatus />
     </main>
   );
 }
